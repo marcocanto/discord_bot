2d0a959dd2b66ef86537308ef5851c94ba713051
Marco Canto <mako.canto@gmail.com>
2021-03-30 20:16:09 -0700

    refreshed token when invalid

--- bot/main.py
+++ bot/main.py
@@ -3,30 +3,29 @@ import os
 import requests
 import json
 import re
+# from dotenv import load_dotenv
 
+# load_dotenv()
 client_id = '28753b42e9824af2a5b5277b6dbf3865'
 client_secret = '34eaa15829f04f3ba13b439f61a6d1d4'
 
 client = discord.Client()
-code='AQD-ORMrz71z-HwlT36QL79ElnFw6oIQ9JRnacrtZINBMkDk210f35_gwolauJz-HpL2UI4LKV6V1oeOibWc-8jqXfglwEalnTXfKpOCOH-LNmmE08Oy2h9q9g7ecp_372oicBdl7P023gM5bckJ7iPPnI43UjRdWaItU4yEMVV5lU3KPu1uT0Hb83R5SUXONQ'
 
 refresh_token = os.getenv('REFRESH_TOKEN')
 API_URL = 'https://accounts.spotify.com/api/token'
-params = {'grant_type': 'refresh_token', 'refresh_token': refresh_token,
+r_params = {'grant_type': 'refresh_token', 'refresh_token': refresh_token,
 'client_id':client_id, 'client_secret': client_secret}
 PLAYLIST_URL = 'https://api.spotify.com/v1/playlists/spotify:playlist:4uzB4Xdaa3xBEqef3FzF0o/tracks'
 SONG_URL = 'https://api.spotify.com/v1/tracks/'
 
-r = requests.post(API_URL, params)
-access_token = r.json().get("access_token")
-
+r = requests.post(API_URL, r_params)
+access_token = r.json().get("access_token") 
 headers = {
     "Authorization": 'Bearer ' + access_token,
     "Content-Type": "application/json"
     }
 
 def get_song(track_id):
-    r = requests.post(PLAYLIST_URL, headers=headers, params=params)
     song = requests.get(SONG_URL + track_id, headers = headers)
     song_json = song.json()
     artist = song_json["artists"][0]["name"]
@@ -54,11 +53,15 @@ async def on_message(message):
                 r = requests.post(PLAYLIST_URL, headers=headers, params=params)
             except requests.exceptions.RequestException as e:  # This is the correct syntax
                 print(e)
+                r = requests.post(API_URL, r_params)
+                access_token = r.json().get("access_token") 
+                r = requests.post(PLAYLIST_URL, headers=headers, params=params)
+                
 
             name, artist = get_song(track_id)
             added_msg = "added **{}** by **{}** to playlist.".format(name, artist)
             await message.channel.send(added_msg)
-            print("Added {} by {} to playlist.".format(name, artist))
+            print(added_msg)
             
 # test
 
