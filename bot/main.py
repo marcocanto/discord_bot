4a48cde5d7f42e1f3917f208cd66a3c2170e15dc
Marco Canto <mako.canto@gmail.com>
2021-03-31 15:01:46 -0700

    updated to add spotipy

--- bot/main.py
+++ bot/main.py
@@ -3,43 +3,27 @@ import os
 import requests
 import json
 import re
+import spotipy
+import spotipy.util as util
 # from dotenv import load_dotenv
 
 # load_dotenv()
 client_id = '28753b42e9824af2a5b5277b6dbf3865'
 client_secret = '34eaa15829f04f3ba13b439f61a6d1d4'
+redirect_url = 'https://example.com/'
+scope = 'playlist-modify-private'
 
 client = discord.Client()
+token = util.prompt_for_user_token(username='marcocanto', redirect_uri=redirect_url, scope=scope, client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(auth=token)
+username = 'marcocanto'
 
-refresh_token = os.getenv('REFRESH_TOKEN')
-API_URL = 'https://accounts.spotify.com/api/token'
-r_params = {'grant_type': 'refresh_token', 'refresh_token': refresh_token,
-'client_id':client_id, 'client_secret': client_secret}
-PLAYLIST_URL = 'https://api.spotify.com/v1/playlists/4uzB4Xdaa3xBEqef3FzF0o/tracks'
-SONG_URL = 'https://api.spotify.com/v1/tracks/'
-
-r = requests.post(API_URL, r_params)
-access_token = r.json().get("access_token") 
-headers = {
-    "Authorization": 'Bearer ' + access_token,
-    "Content-Type": "application/json"
-    }
-
-def refresh_token():
-    r = requests.post(API_URL, r_params)
-    print("Refreshing Token")
-    print(r.content)
-    access_token = r.json().get("access_token") 
-    headers = {
-        "Authorization": 'Bearer ' + access_token,
-        "Content-Type": "application/json"
-        }
+spotify.trace = False
 
 def get_song(track_id):
-    song = requests.get(SONG_URL + track_id, headers = headers)
-    song_json = song.json()
-    artist = song_json["artists"][0]["name"]
-    name = song_json["name"]
+    song = spotify.track(track_id)
+    artist = song["artists"][0]["name"]
+    name = song["name"]
     return name, artist
 
 @client.event
@@ -54,29 +38,19 @@ async def on_message(message):
     if 'spotify' in message.content:
         print("Message Content:")
         print(message.content)
-        refresh_token()
+        # refresh_token()
         embeds = message.embeds
-        if len(embeds) == 1:
-            url = embeds[0].url
-            x = re.findall(r"^(https:\/\/open.spotify.com\/track\/|spotify:user:spotify:playlist:)([a-zA-Z0-9]+)(.*)$",url)
-            track_id = x[0][1]
-            uri = ["spotify:track:" + track_id]
-            params = {"uris": uri}
-            try:
-                print(track_id)
-                r = requests.post(PLAYLIST_URL, headers=headers, params=params)
-                print(r.content)
-            except requests.exceptions.RequestException as e:  # This is the correct syntax
-                print(e)
-                refresh_token()
-                r = requests.post(PLAYLIST_URL, headers=headers, params=params)
-                print(r.content)
-                
-
-            name, artist = get_song(track_id)
-            added_msg = "added **{}** by **{}** to playlist.".format(name, artist)
-            await message.channel.send(added_msg)
-            print(added_msg)
+        url = embeds[0].url
+        x = re.findall(r"^(https:\/\/open.spotify.com\/track\/|spotify:user:spotify:playlist:)([a-zA-Z0-9]+)(.*)$",url)
+        track_id = x[0][1]
+        track_ids = [track_id]
+        results = spotify.user_playlist_add_tracks(username, '4uzB4Xdaa3xBEqef3FzF0o',  track_ids)
+        print(results)
+        name, artist = get_song(track_id)
+        
+        added_msg = "added **{}** by **{}** to playlist.".format(name, artist)
+        print(added_msg)
+        await message.channel.send(added_msg)
             
 # test
 
